9a004ef2aa9148db7f6d1b2f02765b7d00708c95
sarori <salo3678@gmail.com>
2020-03-13 21:48:38 +0100

    .

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root")
+);
